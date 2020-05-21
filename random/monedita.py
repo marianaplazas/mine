2610c7c2c0b865052f49b388db08b0a564193310
marianaplazas <marianaplazas@live.com>
2020-05-21 22:25:54 +0000

    [add] monedita it works

---
+++ random/monedita.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import random
+
+
+def monedita_sueca(floor,ceiling, games):
+	max = len(games)
+	if (floor < 0):
+		print("Be serious")
+	elif (ceiling > max):
+		print("There's no so many games!")
+	else:
+		position = (random.randint(floor, ceiling))
+		print(games[position])
+
+def add_games():
+	games = []
+	while (True):
+		i = (input("Hi add a new game: "))
+		if (i == "Stop"):
+			break
+		else:
+			games.append(i)
+	return(games)
+
+games = add_games()
+if (len(games) < 0):
+	print("Your list games are empty")
+else:
+	floor = int(input("Tell me the min: "))
+	ceiling = int(input("Tell me the max: "))
+	monedita_sueca(floor, ceiling, games)
